cb8bdb64b5860ca768c6f863c1080881803005bf
unknown <upendara628@gmail.com>
2026-02-08 21:51:40 -0800

    Update DSA file

---
+++ 09_DSA.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.link = None
+
+node1 = Node(1)     
+node2 = Node(2)
+node3 = Node(3)
+
+
+
+node1.link = node2
+node2.link = node3
+
+head = node1
+
+
+currentNode = head
+
+while currentNode :
+    print(currentNode.data ,end=" =>")
+    currentNode = currentNode.link
+print("none")    
+
+
